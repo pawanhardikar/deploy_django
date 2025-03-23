12c3c51cd63c0ace0a6633ba1557e7d0c96aacfa
pawanhardikar <pawanhardikar@gmail.com>
2025-03-23 21:13:49 +0530

    Added a comment in views file

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 
-# Create your views here.
+# Create your views here. Hello World For Now
 def say_hello(request):
     return render(request, 'hello.html')
